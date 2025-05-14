d28c1c609ae84ea6942eed4a0165557e60ba2dcc
Nithin-vs <nithin.vs1720@gmail.com>
2025-05-14 21:59:50 +0530

    14-May-25 variable and datatypes

---
+++ 14-May-25/Variables & Datatypes.py
@@ -0,0 +1,8 @@
+patient_name = "John Doe"
+patient_age = 20
+new_patient = True
+
+# Print the variables
+print("Patient Name:", patient_name)
+print("Patient Age:", patient_age)
+print("New Patient:", new_patient)
